d34ce40e326f3eaca7a762fef2f48e6e846d25f3
Matheus Muriel <matheus.muriel@outlook.com>
2020-10-02 20:18:14 -0300

    Load Json

--- scripts.js
+++ scripts.js
@@ -1,6 +1,25 @@
+//var frases;
+//loadJSON((response) => { console.log(JSON.parse(response)[0].frase) });
+
+function loadJSON(callback) {   
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'frases.json', true);
+  xobj.onreadystatechange = () => {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);  
+}
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: ''
+  },
+  beforeCreate() {
+    var vm = this
+    loadJSON((response) => { vm.message = JSON.parse(response)[0].frase });
   }
 })
